f4042c6e5fd57b7822409956c1336fb76b92cae6
Yukun <guoyukn@gmail.com>
2022-06-23 00:26:32 -0700

    first commit

---
+++ src/electron/preload/preload.ts
@@ -0,0 +1,13 @@
+// src/main/preload.ts
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector:any, text:any) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
