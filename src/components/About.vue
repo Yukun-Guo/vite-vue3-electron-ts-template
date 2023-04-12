93419721de73f825ad0728eda4f36a620961e2e4
Yukun <guoyukn@gmail.com>
2023-04-12 13:57:28 -0700

    add example of loading image from assets in router

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,9 @@
+<script setup>
+import imageUrl from "../../src/assets/imgs/about.png";
+</script>
 <template>
-  <h1>About Page</h1>
+  <div style="background-color: beige">
+    <h1>About Page</h1>
+    <img :src="imageUrl" alt="about logo" width="100" height="100" />
+  </div>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,9 @@
+<script setup>
+import imageUrl from "../../src/assets/imgs/home.png";
+</script>
 <template>
-  <h1>Home Page</h1>
+  <div style="background-color: beige">
+    <h1>Home Page</h1>
+    <img :src="imageUrl" alt="home logo" width="100" height="100" />
+  </div>
 </template>
